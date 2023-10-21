b50c9d765da68deadcc7238dfa75cb3a8efa6143
Kaki-student <146790365+Kakiiiiiii@users.noreply.github.com>
2023-10-22 00:12:48 +0900

    uplode

--- 2023_fall/lec02/homework2.py
+++ 2023_fall/lec02/homework2.py
@@ -4,12 +4,10 @@ b = int(7 % 3)
 c = int(55 * 234)
 name1 = "KCGI"
 name2 = "Kyoto"
-d = "KCGI" + ' @ ' + "Kyoto"
 e = round(3.458, 2)
 f = str(57)
 g = 5 == 9
 h = str(-23.0)
 i = float(False)
-i = firstbool = (4 > 5) print("4>5 is ", firstbool)
 j = bool("Python")
 k = "global" + "_squ"
