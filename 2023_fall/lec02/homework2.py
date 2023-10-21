c1774bbeb892e0695f97c8102354551148eb3ea0
Kaki-student <146790365+Kakiiiiiii@users.noreply.github.com>
2023-10-22 00:15:35 +0900

    upload

--- 2023_fall/lec02/homework2.py
+++ 2023_fall/lec02/homework2.py
@@ -2,12 +2,17 @@
 a = "This is a python string"
 b = int(7 % 3)
 c = int(55 * 234)
+b = 7 % 3
+c = 55 * 234
 name1 = "KCGI"
 name2 = "Kyoto"
+d = name1 + ' @ ' + name2
 e = round(3.458, 2)
-f = str(57)
+f = 57
+
 g = 5 == 9
 h = str(-23.0)
 i = float(False)
 j = bool("Python")
 k = "global" + "_squ"
+
